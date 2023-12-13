5e69ef8ee3e8848e2ad3c21659c077803458d323
Alex Dai <60891469+oshiimine@users.noreply.github.com>
2023-12-13 18:51:37 -0500

    V3

--- 7-springmvc/thymeleafdemo/thymeleafdemo/src/main/java/com/oshiimine/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 7-springmvc/thymeleafdemo/thymeleafdemo/src/main/java/com/oshiimine/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -27,33 +27,19 @@ public class HelloWorldController {
 
     @RequestMapping("/processFormV2")
     public String letsShoutDude(HttpServletRequest request, Model model) {
-
-        // read the request parameter from the HTML form
         String theName = request.getParameter("studentName");
-
-        // convert the data to all caps
         theName = theName.toUpperCase();
-
-        // create the message
         String result = "Yo! " + theName;
-
-        // add message to the model
         model.addAttribute("message", result);
 
         return "helloworld";
     }
 
-    @RequestMapping("/processFormVersionThree")
+    @RequestMapping("/processFormV3")
     public String processFormVersionThree(@RequestParam("studentName") String theName,
                                           Model model) {
-
-        // convert the data to all caps
         theName = theName.toUpperCase();
-
-        // create the message
         String result = "Hey My Friend from v3! " + theName;
-
-        // add message to the model
         model.addAttribute("message", result);
 
         return "helloworld";
